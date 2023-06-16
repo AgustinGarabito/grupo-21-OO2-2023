663497330927a32a134a2d0088e165c91cdac5e7
Pat <128170291+Patry-P@users.noreply.github.com>
2023-06-16 09:56:29 -0300

    Hay faltantes en la estructura

---
+++ src/main/java/com/unla/grupo21OO22023/Grupo21Oo22023Application.java
@@ -0,0 +1,13 @@
+package com.unla.grupo21OO22023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo21Oo22023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo21Oo22023Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/unla/grupo21OO22023/repositories/IEventoRepository.java
@@ -0,0 +1,27 @@
+package com.unla.grupo21OO22023.repositories;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.unla.grupo21OO22023.entities.DispositivoIOT;
+import com.unla.grupo21OO22023.entities.Evento;
+
+@Repository("eventoRepository")
+public interface IEventoRepository extends JpaRepository<Evento, Serializable> {
+	
+	public abstract List<Evento> findByDispositivoIOT(DispositivoIOT dispositivoIOT);
+
+	public abstract Evento findByDescripcion(String descripcion);
+
+	public abstract Evento findByFechaAndHora(LocalDate fecha, LocalTime hora);
+
+	@Query("SELECT e FROM Evento e INNER JOIN DispositivoIOT d WHERE"
+			+ "	CONCAT(e.descripcion,e.fecha,e.hora,e.dispositivoIOT)" + "	LIKE %?1%")
+	public abstract List<Evento> findAll(String filtro);
+}
