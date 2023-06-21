95c471dce5a131ec9d9cee8096e49728204b6758
Pat <128170291+Patry-P@users.noreply.github.com>
2023-06-21 18:07:07 -0300

    Controlador de Medicion y rutas

--- src/main/java/com/unla/grupo21OO22023/controllers/MedicionController.java
+++ src/main/java/com/unla/grupo21OO22023/controllers/MedicionController.java
@@ -1,5 +1,36 @@
 package com.unla.grupo21OO22023.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.unla.grupo21OO22023.helpers.ViewRouteHelper;
+import com.unla.grupo21OO22023.services.IMedicionService;
+
+@Controller
+@PreAuthorize("hasRole('ROLE_ADMIN')")
+@RequestMapping("/medicion")
 public class MedicionController {
+	
+	@Autowired
+	@Qualifier("medicionService")
+	private IMedicionService medicionService;
+
+	//private ModelMapper modelMapper = new ModelMapper();
+
+		
+	@GetMapping("")
+	public ModelAndView index() {
+		ModelAndView mAV = new ModelAndView(ViewRouteHelper.MEDICION_INDEX);
+		mAV.addObject("listaMediciones", medicionService.getAll());		
+		return mAV;
+	}
+	
+	
+	
 
 }
--- src/main/java/com/unla/grupo21OO22023/helpers/ViewRouteHelper.java
+++ src/main/java/com/unla/grupo21OO22023/helpers/ViewRouteHelper.java
@@ -24,10 +24,27 @@ public class ViewRouteHelper {
 	public final static String EVENTO_ERROR = "evento/error";
 	
 	
+	//MEDICION
+	public final static String MEDICION_INDEX = "medicion/listaMediciones";
+	
+	//ALUMBRADO
+	public final static String ALUMBRADO_INDEX = "alumbrado/listaAlumbrado";
+	public final static String ALUMBRADO_NEW = "alumbrado/new";
+	public final static String ALUMBRADO_UPDATE = "alumbrado/update";
+	public final static String ALUMBRADO_LIST = "alumbrado/listaAlumbrado";
+	public final static String ALUMBRADOYEVENTO_LIST = "alumbrado/listaEventosAlumbrado";
+	
+	
 	// REDIRECTS
 	public final static String ROUTE = "/index";
 	public final static String RIEGO_ROOT = "/riego";
 	public final static String EVENTOS_SINGLE = "/evento/riegos/";
 	public final static String R_EVENTO_ERROR = "/evento/error";
+	
+	
+	public final static String ALUMBRADO_ROOT = "/alumbrado";
+	public final static String EVENTO_ROOT = "/alumbrado/eventos";
+	public final static String MEDICION_ROOT = "/medicion";
+	
 	
 }
